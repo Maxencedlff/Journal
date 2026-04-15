4a64779de97e6b3900f6869725d5a69d20b45688
Maxence <maxdelafouch@gmail.com>
2026-04-16 00:03:39 +0200

    Bump SW cache to v2 to force full reload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'journal-v1';
+const CACHE = 'journal-v2';
 const ASSETS = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json', '/icon.svg'];
 
 self.addEventListener('install', e => {
